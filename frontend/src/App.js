955255b249b0c43f182fe13d4a47f528d4311455
Harish Rana <r.shivi@yahoo.com>
2019-12-02 23:47:19 +0530

    Setup Monorepo and add husky, linting.
    This commit sets up monorepo and add eslint and husky to it.
    It also removes the backend boilerplate generated vis express-generator.

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line import/no-unresolved
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Initial Zapier
+      </p>
+    </header>
+  </div>
+);
+
+export default App;
