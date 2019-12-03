f8fa7e05bda112b6cf2efc3aa0b651a92a6fba91
Mohammad Wasi <mdwasi15@gmail.com>
2019-12-03 19:57:40 +0530

    merge to master

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express()
+
+
+console.log('es6 features works')
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.listen(3000, () => console.log('App listening on port 3000!'));
+
