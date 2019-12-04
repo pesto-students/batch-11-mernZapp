e0edb492ab91d8051a67b464379194263ff7177b
Harish Rana <r.shivi@yahoo.com>
2019-12-04 11:59:08 +0530

    Change index json function to es6 arrow function
    This commit rebases this branch's commit on the master,
    It also fixes some linting errors on new code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,9 @@
-import express from 'express'
-const app = express()
+import express from 'express';
 
-
-console.log('es6 features works')
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
+const app = express();
+console.log('es6 features works');
+app.get('/', (_req, res) => {
+  res.send('hello world');
+});
 
 app.listen(3000, () => console.log('App listening on port 3000!'));
-
