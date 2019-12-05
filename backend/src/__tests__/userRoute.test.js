6c46913a8a84966d5a5513dd6f4e49ac7f60cf67
Harish Rana <r.shivi@yahoo.com>
2019-12-05 18:31:47 +0530

    feat: Add authentication to the app
    This commit also does the following things:
    - adds the authentication system
    - changes the static preloading environment variable
    - adds supertest to test the apis

---
+++ backend/src/__tests__/userRoute.test.js
@@ -0,0 +1,69 @@
+import request from 'supertest';
+import app from '../index';
+
+describe('User authentication flow', () => {
+  it('should create a new user', async () => {
+    const res = await request(app)
+      .post('/users/create')
+      .send({
+        email: 'demo1@gmail.com',
+        password: 'harish',
+      });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('user');
+    const { token } = res.body;
+    const resDelete = await request(app)
+      .delete('/users/me').set('Authorization', `Bearer ${token}`);
+    expect(resDelete.statusCode).toEqual(200);
+  });
+
+  it('should create a new user and log-in and logout', async () => {
+    const res = await request(app)
+      .post('/users/create')
+      .send({
+        email: 'demo3@gmail.com',
+        password: 'harish',
+      });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('user');
+
+    const resLogin = await request(app)
+      .post('/users/login')
+      .send({
+        email: 'demo3@gmail.com',
+        password: 'harish',
+      });
+    expect(resLogin.statusCode).toEqual(200);
+    expect(resLogin.body).toHaveProperty('user');
+    const tokenNew = resLogin.body.token;
+
+    const resLogout = await request(app)
+      .post('/users/logout')
+      .set('Authorization', `Bearer ${tokenNew}`)
+      .send();
+    expect(resLogout.statusCode).toEqual(200);
+
+    const resLogoutWithoutLogin = await request(app)
+      .post('/users/logout')
+      .set('Authorization', `Bearer ${tokenNew}`)
+      .send();
+    expect(resLogoutWithoutLogin.body.error).toEqual('please authenticate');
+
+    const resLogin2 = await request(app)
+      .post('/users/login')
+      .send({
+        email: 'demo3@gmail.com',
+        password: 'harish',
+      });
+    expect(resLogin2.statusCode).toEqual(200);
+    expect(resLogin2.body).toHaveProperty('user');
+    const tokenLogin2 = resLogin2.body.token;
+
+    const resDelete = await request(app)
+      .delete('/users/me').set('Authorization', `Bearer ${tokenLogin2}`);
+    expect(resDelete.statusCode).toEqual(200);
+    // delete that user
+    afterEach(async () => {
+    });
+  });
+});
---
+++ backend/src/models/userModel.js
@@ -0,0 +1,87 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { JSON_WEB_SECRET_KEY, SALT_WORK_FACTOR } from '../config';
+
+// TODO: handle password, don't save token.
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  tokens: [{
+    token: {
+      type: String,
+      required: true,
+    },
+  }],
+}, {
+  timestamps: true,
+});
+
+userSchema.methods.toJSON = function toJSON() {
+  const user = this;
+  const userObject = user.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+  delete userObject._id;
+  delete userObject.createdAt;
+  delete userObject.__v;
+
+  return userObject;
+};
+
+userSchema.statics.findByCredentials = async function findByCredentials(email, password) {
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw new Error('User not found');
+  }
+
+  const isPasswordMatch = await this.comparePassword(password, user.password);
+
+  if (!isPasswordMatch) {
+    throw new Error('Credentials not right');
+  }
+  return user;
+};
+
+userSchema.pre('save', async function hashAndSavePassword(next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, SALT_WORK_FACTOR);
+  }
+  next();
+});
+
+userSchema.statics.comparePassword = async function comparePassword(candidatePassword, storedHash) {
+  return bcrypt.compare(candidatePassword, storedHash);
+};
+
+userSchema.methods.generateAuthToken = async function generateAuthToken() {
+  const user = this;
+  const token = jwt
+    .sign({ _id: user._id.toString() }, JSON_WEB_SECRET_KEY);
+  user.tokens = user.tokens.concat({
+    token,
+  });
+  await user.save();
+  return token;
+};
+
+const User = mongoose.model('User', userSchema);
+export default User;
