189840bdb91a1a1e4c849f52eba5e88140bb94b1
Harish Rana <r.shivi@yahoo.com>
2019-12-06 21:34:58 +0530

    feat: add login api

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,25 +1,28 @@
-import User from '../models/user';
-
+import User from '../models/userModel';
 
 const userLogin = async (req, res) => {
   try {
     const user = await User.findByCredentials(req.body.email, req.body.password);
+    // console.log(user);
     const token = await user.generateAuthToken();
     res.send({ user, token });
   } catch (error) {
-    res.status(500).send('Error in loggin api');
+    console.log(error);
+    // res.status(400).send('Error in loggin api', error);
   }
 };
 
 const userCreate = async (req, res) => {
-  const user = new User(req.body);
+  // console.log(req.body, User);
 
+  const user = new User(req.body);
   try {
     await user.save();
     const token = await user.generateAuthToken();
     res.send({ user, token });
   } catch (error) {
-    res.status(500).send('Error in creating user');
+    console.log(error);
+    res.status(400).send('Error in creating user');
   }
 };
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import initDb from './db/mongoose';
-import userRouter from './routers/user';
+import userRouter from './routers/userRouter';
 
 const app = express();
 
@@ -8,10 +8,5 @@ initDb();
 
 app.use(express.json());
 app.use(userRouter);
-
-console.log('es6 features works');
-app.get('/', (_req, res) => {
-  res.send('hello world');
-});
 
 app.listen(3000, () => console.log('App listening on port 3000!'));
