8be8d40ed85be1a7040982c5e9180d7bdd7a4c30
jatin33 <jatinpanjwani12@gmail.com>
2019-12-06 21:42:24 +0530

    Updated mongoose file

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const initDb = () => {
-  mongoose.connect(process.env.DATABASE_URL, {
+  mongoose.connect('mongodb://127.0.0.1:27017/mernzapp', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
