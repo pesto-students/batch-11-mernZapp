ac9f73593bfbdc6659f3cf1851d2076c703ac657
Mohammad Wasi <mdwasi15@gmail.com>
2019-12-13 20:53:50 +0530

    fix conflicts

--- backend/src/slack-api/routes/routes.js
+++ backend/src/slack-api/routes/routes.js
@@ -1,29 +1,25 @@
 import slackApi from '../service';
+import controller from '../controller';
 
 const express = require('express');
 
-const router = express.Router();
+const slackRouter = new express.Router();
 
-router.get('/slack', (req, res) => {
+slackRouter.get('/slack', (req, res) => {
   const integUrl = slackApi.loginSlack();
   res.render('slackIinteg', {
-    // eslint-disable-next-line object-shorthand
-    integUrl: integUrl,
+    integUrl,
   });
 });
 
-router.get('/auth/events', (req, res) => {
-  console.log(req.body);
+slackRouter.get('/mernZapp/slack/auth/redirect', (req, res) => {
+  controller.authRedirectCallback(req, res);
+});
+
+slackRouter.post('/slack/events', (req, res) => {
+  controller.postTochannel(req.body);
   const { challenge } = req.body;
   res.send(challenge);
 });
 
-router.get('/mernZapp/slack/auth/redirect', async (req, res) => {
-  console.log(req.query);
-  const { code } = req.query;
-  const authCallbackResponse = await slackApi.getUserData(code);
-  res.send(authCallbackResponse);
-//   slackApi.loginSlackCallback(req, res);
-});
-
-export default router;
+export default slackRouter;
