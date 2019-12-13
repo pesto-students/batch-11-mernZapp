9ee53b7b17558110d8f856149d6e72a542f877e1
Harish Rana <r.shivi@yahoo.com>
2019-12-13 11:49:41 +0530

    feat: Add github service and related apis
    This commit add github service static data and adds the APIs to access that.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,22 @@
 import express from 'express';
 import initDb from './db/mongoose';
 import userRouter from './routers/userRouter';
-import './config/index';
+import servicesRouter from './routers/servicesRouter';
+import { NODE_ENV } from './config/index';
 
 const app = express();
 
 app.use(express.json());
 app.use(userRouter);
+app.use(servicesRouter);
 
 
 initDb().then(() => {
-  app.listen(3000, () => {
-    console.log('App listening on port 3000!');
-  });
+  if (NODE_ENV !== 'test') {
+    app.listen(3000, () => {
+      console.log('App listening on port 3000!');
+    });
+  }
 }).catch(error => {
   console.log(error);
 });
