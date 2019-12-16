e2e17145c40fb712d75ff54d3dc2f0af38b4358f
Mohammad Wasi <mdwasi15@gmail.com>
2019-12-16 16:45:14 +0530

    signin/signup integration

--- frontend/src/containers/signUp/SignUp.js
+++ frontend/src/containers/signUp/SignUp.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import { register } from '../../actions/auth';
 import {
   Wrapper,
   Form,
@@ -11,7 +14,34 @@ import {
 } from '../../components/FormCss';
 import ZappTextField from '../../components/TextField';
 
-export default function SignUp() {
+// eslint-disable-next-line no-shadow
+const Register = ({ register, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  // eslint-disable-next-line object-curly-newline
+  const { username, email, password, password2 } = formData;
+
+  const handleChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      // setAlert('Passwords do not match', 'danger');
+    } else {
+      // console.log(formData);
+      register({ username, email, password });
+    }
+  };
+
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -26,6 +56,7 @@ export default function SignUp() {
             name="username"
             type="text"
             autoComplete="username"
+            handleChange={handleChange}
           />
           <ZappTextField
             id="email"
@@ -33,6 +64,7 @@ export default function SignUp() {
             name="email"
             type="email"
             autoComplete="email"
+            handleChange={handleChange}
           />
           <ZappTextField
             id="password"
@@ -40,19 +72,22 @@ export default function SignUp() {
             name="password"
             type="password"
             autoComplete="current-password"
+            handleChange={handleChange}
           />
           <ZappTextField
             id="confirm-password"
             label="Confirm Password"
-            name="confirm-password"
+            name="password2"
             type="password"
             autoComplete="current-password"
+            handleChange={handleChange}
           />
           <StyledButton
             type="submit"
             fullWidth
             variant="contained"
             color="primary"
+            onClick={onSubmit}
           >
             Sign Up
           </StyledButton>
@@ -65,4 +100,21 @@ export default function SignUp() {
       </Wrapper>
     </Container>
   );
-}
+};
+
+Register.propTypes = {
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
+};
+Register.defaultProps = {
+  isAuthenticated: null,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(
+  mapStateToProps,
+  { register },
+)(Register);
