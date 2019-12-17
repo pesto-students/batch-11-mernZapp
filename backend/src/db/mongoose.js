1b83e8846af084c7d55037d2c7b0e58ed069aabe
jatin33 <jatinpanjwani12@gmail.com>
2019-12-17 19:39:11 +0530

    Added in-memorydb config file

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import { DATABASE_URL } from '../config';
 
-const initDb = () => mongoose.connect(DATABASE_URL, {
+const initDb = () => mongoose.connect(process.env.MONGO_URL || DATABASE_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
