a6089c6b9d0aa33d53ec853b06cc25d5eb81943d
Mohammad Wasi <mdwasi15@gmail.com>
2019-12-18 18:40:45 +0530

    feat: Complete landing page

--- frontend/src/components/StyledButton/index.js
+++ frontend/src/components/StyledButton/index.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
 
-const ZappStyledButton = styled(Button)`
-  margin: ${(props) => props.margin};
-  color: ${(props) => props.color};
-  @media (max-width: 768px) {
-    padding: 6px 10px 3px;
-    font-size: 0.8rem;
-    line-height: 1;
-    height: max-content;
-    margin-left: 0.6rem;
-  }
-`;
 
 const ZappButton = (props) => {
   const { margin, color, text } = props;
 
   return (
-    <ZappStyledButton color={color} variant="contained" margin={margin}>{text}</ZappStyledButton>
+    <Button className="ZappButton" variant="contained" color={color} margin={margin}>{text}</Button>
   );
 };
 ZappButton.propTypes = {
--- frontend/src/components/Subheading.js
+++ frontend/src/components/Subheading.js
@@ -2,23 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+
 const SubheadingStyle = styled.h3`
-  border-bottom: 1px solid #000;
+  border-bottom: ${(props) => props.border || '1px solid #000'};
   line-height: 0.9em;
   width: fit-content;
 `;
 
 const Subheadings = (props) => {
-  const { float, text } = props;
+  const { float, text, border } = props;
 
   return (
-    <SubheadingStyle float={float}>{text}</SubheadingStyle>
+    <SubheadingStyle float={float} border={border}>{text}</SubheadingStyle>
   );
 };
 
 Subheadings.propTypes = {
   float: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
+  border: PropTypes.string,
+};
+Subheadings.defaultProps = {
+  border: '1px solid #000',
 };
 
 export {
