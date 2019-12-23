487b273b4996c8e0da314dd5d08a3a840a1383b6
Harish Rana <r.shivi@yahoo.com>
2019-12-23 17:24:01 +0530

    fix: solve cors

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -4,10 +4,9 @@ const router = new express.Router();
 
 router.use((req, res, next) => {
   if (req.headers.origin !== undefined) {
-    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, *');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
   }
   next();
 });
